72530c0ca30556d63fb6d6a9d2f797298686b808
koed <koed@L097105.corp.ads>
2020-04-28 12:45:28 -0400

    ver1

--- src/main/java/com/afweb/service/WebAppConfig.java
+++ src/main/java/com/afweb/service/WebAppConfig.java
@@ -29,27 +29,27 @@ public class WebAppConfig {
         }
         if (CKey.SQL_DATABASE == CKey.MYSQL) {
           
-//eddy corp            
 //sh-4.2$ env | grep MYSQL
 //MYSQL_PREFIX=/opt/rh/rh-mysql57/root/usr
 //MYSQL_VERSION=5.7
 //MYSQL_DATABASE=sampledb
 //MYSQL_PASSWORD=admin
 //MYSQL_PORT_3306_TCP_PORT=3306
-//MYSQL_PORT_3306_TCP=tcp://100.65.146.239:3306
+//MYSQL_PORT_3306_TCP=tcp://100.65.134.53:3306
 //MYSQL_SERVICE_PORT_MYSQL=3306
 //MYSQL_PORT_3306_TCP_PROTO=tcp
-//MYSQL_PORT_3306_TCP_ADDR=100.65.146.239
+//MYSQL_PORT_3306_TCP_ADDR=100.65.134.53
 //MYSQL_SERVICE_PORT=3306
 //MYSQL_USER=sa
 //MYSQL_ROOT_PASSWORD=admin
-//MYSQL_PORT=tcp://100.65.146.239:3306
-//MYSQL_SERVICE_HOST=100.65.146.239
+//MYSQL_PORT=tcp://100.65.134.53:3306
+//MYSQL_SERVICE_HOST=100.65.134.53
 //sh-4.2$
 
 
+
             dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-            String dsUrl = "jdbc:mysql://100.65.146.239:3306/sampledb?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=false";
+            String dsUrl = "jdbc:mysql://100.65.134.53:3306/sampledb?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=false";
             dataSource.setUrl(dsUrl);
             dataSource.setUsername("sa");
             dataSource.setPassword("admin");
